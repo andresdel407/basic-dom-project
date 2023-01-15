0a4434582ebbc589298e66c573026309514cc99f
andresdel407 <andresh407@yahoo.com.co>
2023-01-15 18:25:07 -0500

    First commit of the project

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log('Escribe algo aquí');
+
+const h1 =  document.querySelector('h1');
+// const parrafo =  document.querySelector('p');
+// const parrafito =  document.querySelector('.parrafo');
+// const pid =  document.querySelector('#pid');
+const calculo1 =  document.getElementById('calculo1');
+const calculo2 =  document.getElementById('calculo2');
+const btn = document.getElementById('btnCalcular');
+const pResult = document.getElementById('result');
+
+btn.addEventListener('click', btnOnclick)
+
+function btnOnclick(){
+    
+}
+
+
+
+// console.log({
+//     h1,
+//     parrafo,
+//     parrafito,
+//     pid,
+//     input
+// });
+
+// // Es mejor pracitca el innerText en lugar del innerHTML, por temas de seguridad
+// h1.innerText = 'Cambiamos el titulo del h'
+
+// h1.setAttribute('class','rojo');
+
+// const img = document.createElement('img');
+// img.setAttribute('src','https://www.northropgrumman.com/wp-content/uploads/Carina-Nebula-nasa-james-webb-space-telescope-home.jpg');
+
+// pid.append(img);
